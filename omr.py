638c3f893635b94937b78e24c366cb24ca485def
mehdi <realhillboy@gmail.com>
2016-05-24 17:08:13 +0430

    Tested on 4

--- omr.py
+++ omr.py
@@ -26,33 +26,35 @@ FILTER_SIZE = 4
 
 
 
-def separate_columns(p, arr, row=True):
+def separate_columns(arr, is_row=True):
     i = 0
     arr = np.array(arr)
-    # if not row:
-    #     arr = (arr > np.max(arr)/10.) * arr
+    if is_row:
+        arr = (arr > np.mean(arr)/3.) * arr
     res = []
     while i < len(arr):
         if arr[i]==0:
             if sum(arr[i:])>0:
-                idx = np.where(arr[i:]>0)[0][0]
+                l = np.where(arr[i:]>0)[0][0]
             else:
-                idx = len(arr)
-            if row:
-                thresh = len(arr)/250.
+                l = len(arr)
+                
+            if is_row:
+                thresh = len(arr)/300.
             else:
-                thresh = len(arr)/100.
-            if idx> thresh or row:
+                thresh = len(arr)/70.
+                
+            if l > thresh:# or is_row:
                 res.append(i)
-                res.append(i + idx)
-            i += idx
+                res.append(i + l)
+            i += l
         else:
             i += 1
 #        print i
     idxs = []
     for i in range(1, len(res)-1, 2):
-        idxs.append([res[i], res[i+1]])
-    if row:
+        idxs.append((res[i], res[i+1]))
+    if is_row:
         rows = []
         for idx in idxs:
             rows.append(p[idx[0]:idx[1], :])
@@ -60,69 +62,78 @@ def separate_columns(p, arr, row=True):
     else:
         cols = []
         for idx in idxs:
-            cols.append(p[:, idx[0]:idx[1]])
+            cols.append(p2[:, idx[0]:idx[1]])
         return cols, idxs
 
 
-img = rgb2gray(imread('1102.jpg'))[177:960, 14:706]
-# p = rgb2gray(imread('1142_2.jpg'))[177:960, 14:706]
-# p = rgb2gray(imread('1208(D).jpg'))[200:1435, :]
+#img = rgb2gray(imread('1102.jpg'))[177:960, 14:706]
+#img = rgb2gray(imread('1142_2.jpg'))[177:960, 14:706]
+
+#img = rgb2gray(imread('1208(D).jpg'))[200:1435, :]
+#img = rgb2gray(imread('1276(C).jpg'))[200:1435, :]
+
+img = rgb2gray(imread('as3.jpg'))[320:818, 75:1104]
+
+
 ##################
 # Separate answers
-tmp = dilation(erosion(~img, disk(FILTER_SIZE)), disk(FILTER_SIZE))
-p = tmp > 10
+s = max(img.shape)
+if s<1000:
+    FILTER_SIZE = s/180.
+elif 1000<s<1200:
+    FILTER_SIZE = s/180.
+elif s>1200.:
+    FILTER_SIZE = s/50.
+
+tmp = erosion(1-img, disk(FILTER_SIZE))
+p = tmp > 30
+
+tmp = dilation(tmp, disk(FILTER_SIZE))
+p2 = tmp > 30
 # p = img_as_bool(tmp)
-FILTER_SIZE = p.shape[0]/400.
+
 
 #########################
 # Separate border of test
 m, n = p.shape
-#rows_diff = [0]*m
+rows_diff = [0]*m
 cols_diff = [0]*n
 
 for i in range(m-1):
     for j in range(n-1):
-#        rows_diff[i] += abs(p[i, j] - p[i, j+1])
-        cols_diff[j] += abs(p[i, j] - p[i+1, j])
+        rows_diff[i] += abs(p[i, j] - p[i, j+1])
+        cols_diff[j] += abs(p2[i, j] - p2[i+1, j])
 
 
-cols, col_idx = separate_columns(p, cols_diff, row=False)
-#rows, row_idx = separate_columns(rows_diff, row=True)
-
-#n_cols = 0
-#min_bound = rows[0].shape[1]
-#max_bound = 0
+cols, col_idx = separate_columns(cols_diff, is_row=False)
+rows, row_idx = separate_columns(rows_diff, is_row=True)
 
 
-rows_diffs = []
-rows = []
-rows_idx = []
-for c in cols:
-    rows_diff = [0]*m
-    for i in range(c.shape[0]-1):
-        for j in range(c.shape[1]-1):
-            rows_diff[i] += abs(c[i, j] - c[i, j+1])
-    r, r_idx = separate_columns(c, rows_diff, row=True)
-#    rows.append(r)
-    rows_idx.append(r_idx)
-    rows_diffs.append(rows_diff)
-    
-H = []
-for i in range(len(rows_idx)):
-    H.append(np.mean([j[1]-j[0] for j in rows_idx[i]]))
-height = np.mean(H)
+n_cols = 0
+min_bound = rows[0].shape[1]
+max_bound = 0
 
-for c in cols:
-    c.reshape(())
-
+# for row in rows:
+#    diff = 0
+#    for i in range(row.shape[1]-1):
+#        r = row.shape[0]/2
+#        diff += abs(row[r, i+1] - row[r, i])
+#    if n_cols < diff/2:
+#        n_cols = diff/2
+#    ones = np.where(row[r]==1)[0]
+#    if ones[0] < min_bound:
+#        min_bound = ones[0]
+#    if ones[-1] > max_bound:
+#        max_bound = ones[-1]
+#    n_cols.append(diff/2)
 
 n_cols = len(cols)
-#n_rows = len(rows)
+n_rows = len(rows)
 
 ####################
 # Separate questions
 width = np.mean(np.array([c.shape[1] for c in cols]))
-#height = np.mean(np.array([r.shape[0] for r in rows]))
+height = np.mean(np.array([r.shape[0] for r in rows]))
 
 
 
@@ -131,23 +142,23 @@ width = np.mean(np.array([c.shape[1] for c in cols]))
 #     for i in range(n_cols):
 #         q.append(r[:, int(i*width):int((i+1)*width)])
 
-#questions = []
-#for c in cols:
-#    for idx in row_idx:
-#        questions.append(c[idx[0]:idx[1], :])
-#
-#
-#predict = []
-#for q in questions:
-#    max_area = 0
-#    max_idx = -1
-#    l = q.shape[1]
-#    for i in range(4):
-#        area = np.sum(q[:, i*l/4:(i+1)*l/4])
-#        if area > max_area:
-#            max_area = area
-#            max_idx = i + 1
-#    predict.append(max_idx)
+questions = []
+for c in cols:
+    for idx in row_idx:
+        questions.append(c[idx[0]:idx[1], :])
+
+
+predict = []
+for q in questions:
+    max_area = 0
+    max_idx = -1
+    l = q.shape[1]
+    for i in range(4):
+        area = np.sum(q[:, i*l/4:(i+1)*l/4])
+        if area > max_area:
+            max_area = area
+            max_idx = i + 1
+    predict.append(max_idx)
 
 
 
